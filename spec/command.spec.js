c13cd0fb3dca0b6490cb412e99451f3c7a6206cc
metracy <matthew.etracy@gmail.com>
2024-07-20 18:22:47 -0500

    command implemented, installed jest runner on fixed build.  WSL2 was causing issues.

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,12 +1,32 @@
-const Command = require('../command.js');
-
+let Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
+
 describe("Command class", function() {
 
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+// command test-1
+
+  test("throws error if command type is NOT passed into constructor as the first parameter", function () {
+    // carrie's video rewritten easier to read
+    let instantiateCommand = function() { new Command();};
+    expect(instantiateCommand).toThrow(new Error('Command type required.'));
   });
 
+// command test-2
+
+  test("constructor sets command type", function() {
+    let modeCommand = 'MODE_CHANGE';
+    let command = new Command(modeCommand);
+    // check assigned correctly as command type object
+    expect(command.commandType).toBe(modeCommand);
+  });
+
+// command test-3
+  test("constructor sets a value passed in as the 2nd argument", function() {
+    // use example from instruction task 2
+    let value = 12000;
+    let command = new Command('MOVE', value);
+    expect(command.value).toBe(value);
+  });
 });
