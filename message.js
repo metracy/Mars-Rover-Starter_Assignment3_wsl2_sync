a4d4e2757e6b4d057c5740bae5dc9f8d6380d73e
metracy <matthew.etracy@gmail.com>
2024-07-20 18:33:22 -0500

    message test 4-5, message.js code

--- message.js
+++ message.js
@@ -1,5 +1,12 @@
 class Message {
-   // Write code here!
-}
+   constructor(name, commands) {
+     if (!name) {
+       throw new Error('Name required.');
+     }
+     // set object names from index 0 and index 1
+     this.name = name;
+     this.commands = commands;
+   }
+ }
 
 module.exports = Message;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,32 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+/*
+Test 4
+
+This test description is “throws error if a name is NOT passed into the constructor as the first parameter”. Review the first test in command.spec.js for an example of how this test works.
+
+Look at the code in command.js. Use that to help you write the Message class in message.js so that your test passes. Refer to the Message Class description above for more details.
+*/
+    // message test 4
+    test("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        let instantiateMessage = function() { new Message();};
+        expect(instantiateMessage).toThrow(new Error('Name required.'));
+    });
+/*
+Test 5
+
+The description is “constructor sets name”. The test confirms that the constructor in the Message class correctly sets the name property in a new message object.
+*/  
+
+    // message test 5
+    test("constructor sets name", function() {
+        // use example message_text
+        let message_text = 'Test message'
+        // intantiate message with message_text.
+        let message = new Message(message_text);
+        // check if name property of object message was assigned correctly
+        expect(message.name).toBe(message_text);
+      });
 
 });
