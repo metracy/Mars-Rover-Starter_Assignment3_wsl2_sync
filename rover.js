dc4d06231733626aaa739ac3b39fec0e38cc5c10
metracy <matthew.etracy@gmail.com>
2024-07-20 18:47:56 -0500

    easy part of class rover constructor added.

--- rover.js
+++ rover.js
@@ -1,5 +1,14 @@
 class Rover {
-   // Write code here!
+   // easy part - just take in position and message and set default values
+   constructor(position, message) {
+      this.position = position;
+      this.mode = 'NORMAL';
+      this.generatorWatts = 110;
+   }
+
+   receiveMessage(message) {
+      
+   }
 }
 
 module.exports = Rover;
