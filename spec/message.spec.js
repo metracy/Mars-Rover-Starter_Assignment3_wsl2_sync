7971fb69c0d97be295d9d1b2325fdece6e2d80da
metracy <matthew.etracy@gmail.com>
2024-07-20 18:38:50 -0500

    message tests 4-6

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -33,4 +33,20 @@ The description is “constructor sets name”. The test confirms that the const
         expect(message.name).toBe(message_text);
       });
 
+/*
+Test 6
+
+The description reads “contains a commands array passed into the constructor as the 2nd argument”. This test confirms that the commands property of a new message object contains the data passed in from the Message(name, commands) call.
+
+*/  
+      test("contains a commands array passed into the constructor as the 2nd argument", function() {
+        // using example from instruction sheet, instantiate commands array
+        let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+        // use example message_text
+        let message_text = 'Test message with two commands'
+        // intantiate message with message_text and array of commands.
+        let message = new Message(message_text, commands);
+        // check if command property of object message was assigned correctly
+        expect(message.commands).toBe(commands);
+      });
 });
