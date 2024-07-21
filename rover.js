b815cd917d9c2a1dc349ea60d67f2a769f1168be
metracy <matthew.etracy@gmail.com>
2024-07-20 19:42:57 -0500

    test 1 with constructor testing default values for rover

--- rover.js
+++ rover.js
@@ -5,9 +5,33 @@ class Rover {
       this.mode = 'NORMAL';
       this.generatorWatts = 110;
    }
+/*
+Command	      |            Value sent with command	                     |            Updates to Rover object	|  Result returned
+MOVE	         |Number representing the position the rover should move to. |	                position	         |  {completed: true}
+STATUS_CHECK	|No values sent with this command.	                        |              No updates              |  {completed: true, roverStatus: {mode: 'NORMAL', generatorWatts: 110, position: 87382098}} (Values for mode, generatorWatts, position will depend on the current state of the rover.)
+MODE_CHANGE	   |String representing rover mode (see modes)	               |              mode	                  |  {completed: true} 
+*/    
 
    receiveMessage(message) {
+      // start at the beginning for
+      let loop_index = 0;
+      // an array of results. Each element in the array is an object that corresponds to one
+      let results = [];
+
+      while (loop_index < message.commands.length ) {
+         
+         let command = message.commands[loop_index];
+         if (command.commandType == 'MOVE') {
+
+         }
       
+         loop_index += 1;
+      };
+      return {
+         message: message.name,
+         results: results
+      };
+
    }
 }
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -6,8 +6,14 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 
-describe("Rover class", function() {
+describe('Rover class', function() {
+  test('constructor sets position and default values for mode and generatorWatts', function() {
+    let position = 98382;
+    let rover = new Rover(position);
+    expect(rover.position).toBe(position);
+    expect(rover.mode).toBe('NORMAL');
+    expect(rover.generatorWatts).toBe(110);
+  });
 
-  // 7 tests here!
 
 });
