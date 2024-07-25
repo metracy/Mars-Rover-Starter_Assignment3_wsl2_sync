653cc3da512a24cda91a5cc0500f1d6b7a3abef6
metracy <matthew.etracy@gmail.com>
2024-07-25 17:05:40 -0500

    Organization of comments

--- rover.js
+++ rover.js
@@ -1,5 +1,5 @@
 class Rover {
-   // easy part - just take in position and message and set default values
+// easy part - just take in position and message and set default values
    constructor(position, message) {
       this.position = position;
       this.mode = 'NORMAL';
